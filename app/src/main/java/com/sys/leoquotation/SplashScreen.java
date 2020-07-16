183d959b476fc4f418071b254fca0e857b8a73eb
SSB Home <khantkyawhtay@gmail.com>
2020-07-16 22:14:40 +0630

    Fourth Commit

--- app/src/main/java/com/sys/leoquotation/SplashScreen.java
+++ app/src/main/java/com/sys/leoquotation/SplashScreen.java
@@ -8,7 +8,7 @@ import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 4000;
+    private final int SPLASH_DISPLAY_LENGTH = 40000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
